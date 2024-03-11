02c982420e83385718dde52cd5d40fbec29da8f6
LpxsBr <anselmolopes.an@gmail.com>
2024-03-10 23:23:30 -0300

    fix: adjust pos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const toEPLView = (id) => {
 
-
     var codigo = document.getElementById(id).value
 
     console.log(codigo)
@@ -20,6 +19,7 @@ const toEPLView = (id) => {
     let fC = 40
 
     const ETIQ = document.getElementById('Etq')
+    ETIQ.style.position = 'relative'
 
     while(ETIQ.firstChild){
         ETIQ.removeChild(ETIQ.firstChild)
@@ -38,10 +38,10 @@ const toEPLView = (id) => {
             console.log(convertLen(element.substring(1, element.length), fC));
         }
         // ORIENTAÇÃO
-        if (element[0] == 'Z') {
-            if (element[1] == 'B') { ETIQ.style.alignItems = 'flex-end' };
-            if (element[1] == 'T') { ETIQ.style.alignItems = 'flex-start' };
-        }
+        // if (element[0] == 'Z') {
+        //     if (element[1] == 'B') { ETIQ.style.alignItems = 'flex-end' };
+        //     if (element[1] == 'T') { ETIQ.style.alignItems = 'flex-start' };
+        // }
 
         // cod barras B100,405,0,1,3,2,60,B,"#CODIGO#"
         if (element[0] == 'B') {
@@ -50,7 +50,7 @@ const toEPLView = (id) => {
             const c = document.createElement('div');
             c.className = 'bar-code-39';
             c.textContent = barCodeConfig[8];
-            c.style.position = 'relative';
+            c.style.position = 'absolute';
             c.style.fontSize = '1.2rem';
             c.style.bottom = convertLen(barCodeConfig[1], fC);
             c.style.left = convertLen(barCodeConfig[0], fC);
@@ -72,7 +72,7 @@ const toEPLView = (id) => {
             if(rotate == 3){t.style.transform = 'rotate(270deg)' }
             t.className = 'text-content';
             t.textContent = textConfig[8].includes('"') && textConfig[8][0] == '"' && textConfig[8][textConfig[8].length - 1] == '"' ? textConfig[8].replaceAll('"', '') : 'Invalid Text';
-            t.style.position = 'relative';
+            t.style.position = 'absolute';
             t.style.fontSize = '1.2rem';
             ETIQ.appendChild(t);
 
@@ -81,5 +81,30 @@ const toEPLView = (id) => {
     });
 
 }
+
+const eplHighLight = (id) => {
+    var editor = document.getElementById(id).value;
+
+    const info = document.getElementById('info');
+
+    let codigo = editor.split('\n');
+
+    const example = {
+        B: 'Codigo de barras',
+        A: 'Texto',
+        Q: 'Altura da etiqueta',
+        q: 'Largura da etiqueta'
+    }
+    var key = codigo[codigo.length - 1][0]
+    addEventListener('keyup', ()=>{
+        if(typeof(key) != undefined && typeof(key) != 'undefined'){
+            console.log(example[key]);
+            info.textContent = example[key]
+        }
+    })
+    
+    
+    
+} 
 
 console.log('to on, teste')
